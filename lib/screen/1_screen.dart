cd4968cf3f7922c10a28d31db3efc9c8e027e07f
JC <withj1123@gmail.com>
2022-07-30 11:47:23 +0900

    v5

--- lib/screen/1_screen.dart
+++ lib/screen/1_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:go_router_actual/layout/default_layout.dart';
 
 class OneScreen extends StatelessWidget {
@@ -8,7 +9,17 @@ class OneScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       body: Column(
-        children: [],
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              context.pop();
+            },
+            child: Text(
+              'POP',
+            ),
+          ),
+        ],
       ),
     );
   }
